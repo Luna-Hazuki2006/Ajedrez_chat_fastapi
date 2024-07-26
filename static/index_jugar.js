64767ed1f4913f67cefc2771897153c3bdde7584
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-07-26 03:27:32 -0400

    Con el websocket correcto

--- static/index_jugar.js
+++ static/index_jugar.js
@@ -6,8 +6,8 @@ let comidas = document.getElementById('comidas')
 let tipo = {}
 document.querySelector("#ws-id").textContent = client_id;
 let cambio = new Audio("https://ajedrez-chat-fastapi.onrender.com/static/audio/mover.ogg")
-// var ws = new WebSocket(`wss://ajedrez-chat-fastapi.onrender.com/ws/${client_id}`);
-var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+var ws = new WebSocket(`wss://ajedrez-chat-fastapi.onrender.com/ws/${client_id}`);
+// var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
 console.log(ws);
 ws.onmessage = function(event) {
     data = JSON.parse(event.data)
