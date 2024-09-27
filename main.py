ccbef870157ccaf3c314d9c822a9e6e81cd1f34c
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-09-27 19:03:39 -0400

    fe de erratas

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
                                         original=real['original'], 
                                         nueva=real['nuevo'], 
                                         pieza=real['valor'], 
-                                                       nombre=nombre)
+                                        nombre=nombre)
                 # await manager.send_personal_message(json.dumps(real), websocket)
                 await manager.broadcast(json.dumps(real))
 
