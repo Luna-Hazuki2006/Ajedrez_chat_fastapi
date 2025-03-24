1892fa9dcc49b3349f78c8ab0e3c8cbb92e7cc40
Luna-Hazuki2006 <86622512+Luna-Hazuki2006@users.noreply.github.com>
2025-03-24 13:27:18 -0400

    Update index_jugar.js

--- static/index_jugar.js
+++ static/index_jugar.js
@@ -8,8 +8,8 @@ let poder = document.getElementById('poder')
 let tipo = {}
 document.querySelector("#ws-id").textContent = client_id;
 let cambio = new Audio("https://ajedrez-chat-fastapi.onrender.com/static/audio/mover.ogg")
-// var ws = new WebSocket(`wss://ajedrez-chat-fastapi.onrender.com/ws/${client_id}`);
-var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+var ws = new WebSocket(`wss://ajedrez-chat-fastapi.onrender.com/ws/${client_id}`);
+// var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
 console.log(ws);
 ws.onmessage = function(event) {
     data = JSON.parse(event.data)
